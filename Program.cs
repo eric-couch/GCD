ba33a94f437da872f946a04fb909cb9759b78959
Eric Couch <james.couch@faculty.edgetechacademy.edu>
2020-09-17 12:12:36 -0500

    Eric's way of GCD using a while loop

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GCD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int q, r;
+            Console.Write("Enter Number: ");
+            int fNum = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Second Number: ");
+            int sNum = Convert.ToInt32(Console.ReadLine());
+            int N1 = fNum;
+            int N2 = sNum;
+            Console.WriteLine("N1=" + fNum);
+            Console.WriteLine("N2=" + sNum);
+            while (fNum % sNum != 0)
+            {
+                q = (fNum / sNum);
+                r = (fNum % sNum);
+                fNum = sNum;
+                sNum = r;
+                Console.WriteLine(fNum + "/" +sNum + "=" + q + " r " + r );
+            }
+            Console.WriteLine(fNum + "/" + sNum + "=" + (fNum / sNum) + " r " + (fNum % sNum));
+            Console.WriteLine("The GCD of " + N1 + " and " + N2 + " is " + sNum );
+        }
+    }
+}
